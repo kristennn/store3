f142ecc62eb6749c95439560dacce067e925b02a
13949836783@163.com <13949836783@163.com>
2017-07-02 11:14:05 +0800

    add is admin to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def admin_required
+    if !current_user.is_admin
+      redirect_to root_path, warning: "你没有权限"
+    end
+  end
+
 end
