0bc1568e91a1820ff8b4fb28cd5a9928f06e8cec
13949836783@163.com <13949836783@163.com>
2017-08-19 14:21:59 +0800

    add category column to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817091405) do
+ActiveRecord::Schema.define(version: 20170819062101) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20170817091405) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.string   "category"
   end
 
   create_table "reviews", force: :cascade do |t|
